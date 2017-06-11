0a4bf62eb8d6eb9d4dcf8352368fa6ca3e7c45f9
Archinmodi <archinmodi@gmail.com>
2017-06-11 17:35:20 +0530

    Add files via upload

---
+++ #3 v-bind/app.js
@@ -0,0 +1,14 @@
+var app=new Vue({
+  el:'#hello',
+  data:{
+    message:'hello John Deo',
+    title:'hello title!',
+    url:'http://www.hdwallpapers.in/thumbs/2017/despicable_me_3_4k_8k-t1.jpg',
+  }
+});
+
+
+
+/*
+http://www.hdwallpapers.in/thumbs/2017/despicable_me_3_4k_8k-t1.jpg
+*/
